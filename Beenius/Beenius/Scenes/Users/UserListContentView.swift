990b0f8e435c7a4d4cfc67f905daf500dbd080ff
Marcel Salej <marcel.salej@icloud.com>
2019-10-09 20:10:27 +0200

    converted project to clean architecture without storyboards

---
+++ Beenius/Beenius/Scenes/Users/UserListContentView.swift
@@ -0,0 +1,28 @@
+//
+//  UserListContentView.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 09/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import UIKit
+
+class UserListContentView: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Private Methods
+private extension UserListContentView {
+  func setupViews() {
+    // setup background, shadows, etc
+    // call other views setup
+  }
+}
