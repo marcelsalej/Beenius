f617a3f478d333df204dd0be230c313fd7b07023
Marcel Salej <marcel.salej@icloud.com>
2019-10-10 10:13:59 +0200

    UICollectionview extensions

---
+++ Beenius/Beenius/Common/Extensions/UICollectionViewCell+Extra.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewCell+Extra.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 10/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+  /// Returns cell's reuse identifier
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
