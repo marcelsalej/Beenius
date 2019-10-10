6539fc211f33b18787073c8478550c3650dec2ed
Marcel Salej <marcel.salej@icloud.com>
2019-10-10 08:38:36 +0200

    UITableview extensions

---
+++ Beenius/Beenius/Common/Extensions/UITableViewCell+Extra.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell+Extra.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 10/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import UIKit
+
+public extension UITableViewCell {
+  /// Returns cell's reuse identifier
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
