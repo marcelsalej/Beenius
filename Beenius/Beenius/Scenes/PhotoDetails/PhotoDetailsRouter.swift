ffdc26f2f64863ef6c8239d0ca32b013c33bf236
Marcel Salej <marcel.salej@icloud.com>
2019-10-12 19:21:41 +0200

    basic scene setup

---
+++ Beenius/Beenius/Scenes/PhotoDetails/PhotoDetailsRouter.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDetailsRouter.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 12/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import Foundation
+
+protocol PhotoDetailsRoutingLogic {
+}
+
+protocol PhotoDetailsRouterDelegate: AnyObject {
+  
+}
+
+class PhotoDetailsRouter {
+  weak var viewController: PhotoDetailsViewController?
+  weak var delegate: PhotoDetailsRouterDelegate?
+}
+
+// MARK: - Routing Logic
+extension PhotoDetailsRouter: PhotoDetailsRoutingLogic {
+  
+}
---
+++ Beenius/Beenius/Scenes/PhotoDetails/Views/PhotoDetailsContentView.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDetailsContentView.swift
+//  Beenius
+//
+//  Created by Marcel Salej on 12/10/2019.
+//  Copyright © 2019 Marcel Salej. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsContentView: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - Private Methods
+private extension PhotoDetailsContentView {
+  func setupViews() {
+  }
+}
